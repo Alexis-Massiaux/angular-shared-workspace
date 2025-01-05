3ef660e364a962d10dba47cbdf4489b949abe492
Alexis Massiaux <alexis.massiaux@worldline.com>
2025-01-05 19:19:55 +0100

    chore: init eva library
    with ng generate library eva

---
+++ projects/eva/src/lib/eva.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-eva',
+  imports: [],
+  template: `
+    <p>
+      eva works!
+    </p>
+  `,
+  styles: ``
+})
+export class EvaComponent {
+
+}
---
+++ projects/eva/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of eva
+ */
+
+export * from './lib/eva.service';
+export * from './lib/eva.component';
